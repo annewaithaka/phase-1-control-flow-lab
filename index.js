2eb4dbf5e0067839e6646733476cc6d35dc7480f
annewaithaka <annewaithaka68@gmail.com>
2024-06-28 15:05:37 +0300

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result
+  if (distance<2500){
+  if (distance <=400) {
+  result="This one is on me!";
+} else if (distance>400 && distance < 2000){
+  result="That will be twenty bucks." 
+} else if (distance>=2000){
+  result= "I will gladly take your thirty bucks."
+} 
+
+}else {
+  result='No can do.'
+}
+return result;
+}
+  
+  
+function ternaryCheckCity(city){
+  // Write your code here!
+  let result=(city==="NYC")? 'Ok, sounds good.' : "No go."
+  return result;
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+    return "Thank you.";
+      break;
+    default:
+    return "Bye."
+  }
 }
